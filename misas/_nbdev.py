c5633dcaf7181af1dd0540665051550363f51fc9
Liliia <shainberg.lj@gmail.com>
2020-06-10 10:07:24 +0000

    Add bright_series function

--- misas/_nbdev.py
+++ misas/_nbdev.py
@@ -2,14 +2,15 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"dice": "01_local_interpret.ipynb",
+index = {"dice_by_component": "01_local_interpret.ipynb",
          "plot_rotation_series": "01_local_interpret.ipynb",
          "rotation_series": "01_local_interpret.ipynb",
          "plot_rotation": "01_local_interpret.ipynb",
          "plot_crop_series": "01_local_interpret.ipynb",
          "crop_series": "01_local_interpret.ipynb",
          "plot_crop": "01_local_interpret.ipynb",
-         "plot_brightness_series": "01_local_interpret.ipynb"}
+         "plot_brightness_series": "01_local_interpret.ipynb",
+         "bright_series": "01_local_interpret.ipynb"}
 
 modules = ["core.py"]
 
